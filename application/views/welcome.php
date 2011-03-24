c051ea26b5671cb5cba13e3fbe8c78a7eecef31d
Eric Bae <ericbae@gmail.com>
2011-03-24 20:53:32 +1100

    Logging users out of Facebook, if they log in with Facebook account

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,1 +1,14 @@
-Hi, <strong><?php echo $username; ?></strong>! You are logged in now. <?php echo anchor('/auth/logout/', 'Logout'); ?>
+Hi, <strong><?php echo $username; ?></strong>! You are logged in now. 
+
+<?php
+	if( $this->session->userdata('facebook_id'))
+	{
+?>
+		<a href="http://m.facebook.com/logout.php?confirm=1&next=<?php echo site_url('auth/logout'); ?>">logout</a>
+<?php		
+	}
+	else
+	{
+		echo anchor('/auth/logout/', 'Logout');
+	}
+?>
