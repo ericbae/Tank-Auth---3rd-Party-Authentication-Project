b0bf1a34824b4188123104a302df54c1a04d8dad
Eric Bae <ericbae@gmail.com>
2011-04-02 17:45:50 +1100

    Starting Google Authentication on XTA

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -95,6 +95,75 @@ class Auth extends CI_Controller
 			$this->load->view('auth/login_form', $data);
 		}
 	}
+	
+	function login2()
+	{
+		if ($this->tank_auth->is_logged_in()) {									// logged in
+			redirect('');
+
+		} elseif ($this->tank_auth->is_logged_in(FALSE)) {						// logged in, not activated
+			redirect('/auth/send_again/');
+
+		} else {
+			$data['login_by_username'] = ($this->config->item('login_by_username', 'tank_auth') AND
+					$this->config->item('use_username', 'tank_auth'));
+			$data['login_by_email'] = $this->config->item('login_by_email', 'tank_auth');
+
+			$this->form_validation->set_rules('login', 'Login', 'trim|required|xss_clean');
+			$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
+			$this->form_validation->set_rules('remember', 'Remember me', 'integer');
+
+			// Get login for counting attempts to login
+			if ($this->config->item('login_count_attempts', 'tank_auth') AND
+					($login = $this->input->post('login'))) {
+				$login = $this->security->xss_clean($login);
+			} else {
+				$login = '';
+			}
+
+			$data['use_recaptcha'] = $this->config->item('use_recaptcha', 'tank_auth');
+			if ($this->tank_auth->is_max_login_attempts_exceeded($login)) {
+				if ($data['use_recaptcha'])
+					$this->form_validation->set_rules('recaptcha_response_field', 'Confirmation Code', 'trim|xss_clean|required|callback__check_recaptcha');
+				else
+					$this->form_validation->set_rules('captcha', 'Confirmation Code', 'trim|xss_clean|required|callback__check_captcha');
+			}
+			$data['errors'] = array();
+
+			if ($this->form_validation->run()) {								// validation ok
+				if ($this->tank_auth->login(
+						$this->form_validation->set_value('login'),
+						$this->form_validation->set_value('password'),
+						$this->form_validation->set_value('remember'),
+						$data['login_by_username'],
+						$data['login_by_email'])) {								// success
+					redirect('');
+
+				} else {
+					$errors = $this->tank_auth->get_error_message();
+					if (isset($errors['banned'])) {								// banned user
+						$this->_show_message($this->lang->line('auth_message_banned').' '.$errors['banned']);
+
+					} elseif (isset($errors['not_activated'])) {				// not activated user
+						redirect('/auth/send_again/');
+
+					} else {													// fail
+						foreach ($errors as $k => $v)	$data['errors'][$k] = $this->lang->line($v);
+					}
+				}
+			}
+			$data['show_captcha'] = FALSE;
+			if ($this->tank_auth->is_max_login_attempts_exceeded($login)) {
+				$data['show_captcha'] = TRUE;
+				if ($data['use_recaptcha']) {
+					$data['recaptcha_html'] = $this->_create_recaptcha();
+				} else {
+					$data['captcha_html'] = $this->_create_captcha();
+				}
+			}
+			$this->load->view('auth/login_form2', $data);
+		}
+	}	
 
 	/**
 	 * Logout user
